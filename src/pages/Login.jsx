df8c3cd280a6960337a3215636124f84583ac836
Gonzalo German Russi <gonzalorussi2502@gmail.com>
2025-06-15 23:14:57 -0300

    update final login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,10 +4,11 @@ import { db } from '../firebase';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { doc, getDoc, setDoc, serverTimestamp } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
-import img from '../assets/img/login_es.avif';
 import Footer from '../Components/Footer';
 import logo from '../assets/MSILOGO.png'
 import {FaGoogle} from 'react-icons/fa';
+import login from '../assets/img/login.avif'
+
 export default function Login() {
   const navigate = useNavigate();
 
@@ -41,20 +42,31 @@ export default function Login() {
   };
 
   return (
-    <>
-      <div className='flex flex-col items-center bg-gray-900'>
-        <div className='flex flex-col items-center'>
-          <img className='mt-4 w-20 h-24' src={logo} alt="Logo MSI" />
-          <button
-            onClick={handleGoogleLogin}
-            className='flex items-center gap-x-2 my-4 cursor-pointer bg-red-800 hover:bg-red-900 transition p-2 rounded-lg font-semibold text-md text-gray-200'
-          >
-          Iniciar sesión con Google<FaGoogle className="w-6 h-6" />
+  <>
+    <div className='flex flex-col md:flex-row min-h-screen'>
+      {/* Columna izquierda */}
+      <div 
+        className='flex flex-col justify-center items-center w-full md:w-1/3 bg-gray-900 p-8 shadow-lg'
+      >
+        <img className='w-20 h-24 mb-6' src={logo} alt="Logo MSI" />
+        <button
+          onClick={handleGoogleLogin}
+          className='flex items-center gap-x-2 my-2 cursor-pointer bg-red-800 hover:bg-red-900 transition p-3 rounded-lg font-semibold text-md text-gray-200'
+        >
+          Iniciar sesión con Google <FaGoogle className="w-6 h-6" />
         </button>
-        </div>
-        <img className='h-[60vh] md:h-[70vh] lg:h-full object-cover' src={img} alt="login" />
       </div>
-      <Footer />
-    </>
-  );
+
+      {/* Columna derecha con la imagen */}
+      <div className='w-full md:w-2/3 h-64 md:h-auto'>
+        <img 
+          src={login} 
+          alt="imagen del MSI"
+          className='w-full h-full object-cover'
+        />
+      </div>
+    </div>
+    <Footer />
+  </>
+);
 }
---
+++ src/utils/logos.js
@@ -0,0 +1,23 @@
+import AL from '../assets/logos/al.png'
+import BLG from '../assets/logos/al.png'
+import CFO from '../assets/logos/al.png'
+import FLY from '../assets/logos/al.png'
+import GENG from '../assets/logos/al.png'
+import FURIA from '../assets/logos/al.png'
+import G2 from '../assets/logos/al.png'
+import GAM from '../assets/logos/al.png'
+import T1 from '../assets/logos/al.png'
+
+const logos = {
+    al: AL,
+    blg: BLG,
+    cfo: CFO,
+    fly: FLY,
+    geng: GENG,
+    fur: FURIA,
+    g2: G2,
+    gam: GAM,
+    t1: T1
+}
+
+export default logos;
