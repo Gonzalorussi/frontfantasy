f717aecfb63521b9bb4470f1d223e64243ca72c9
Gonzalo German Russi <gonzalorussi2502@gmail.com>
2025-06-16 15:17:21 -0300

    error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import AuthProvider from './context/AuthProvider';
 import './index.css';
+import ErrorBoundary from './Components/ErrorBoundary';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-    <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </AuthProvider>
   </React.StrictMode>,
 );
